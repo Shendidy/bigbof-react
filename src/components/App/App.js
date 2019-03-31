85561648e109215397c90a31f3eb90d028a70e2a
Shendidy <sherif@shendidy.com>
2019-03-31 07:40:31 +0100

    :truck: chore: moved App files to components/App folder

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,21 +7,7 @@ class App extends Component {
     console.log("%cSTOP!\n\n%cPlease don't change anything here unless you know what you're doing.\n\nYou might give hackers access to your account if you're not careful.\n\n\nGood luck,\nbigBOF.com team.", "color: red; font-size: 25px;", "color: blue; font-size: 15px;");
 
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      "bigBOF.com shall be here soon!"
     );
   }
 }
