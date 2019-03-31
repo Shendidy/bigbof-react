9fa27cabef57fe8a0b8f4de29444debded5bbe3e
Shendidy <sherif@shendidy.com>
2019-03-31 07:35:40 +0100

    :speech_balloon: chore: warning in console

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    // Message for people accessing the console after loading the page.
+    console.log("%cSTOP!\n\n%cPlease don't change anything here unless you know what you're doing.\n\nYou might give hackers access to your account if you're not careful.\n\n\nGood luck,\nbigBOF.com team.", "color: red; font-size: 25px;", "color: blue; font-size: 15px;");
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
