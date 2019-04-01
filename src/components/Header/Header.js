3c98d514062c77688268a9079ea4f83656d8af27
Shendidy <sherif@shendidy.com>
2019-04-01 12:06:41 +0100

    :lipstick: style: rearranged divs

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,10 +5,10 @@ import text from '../../languages/text';
 function Header(props) {
   return(
     <div>
-      <div className="headerDiv">
-        <div className="logoDiv">
-          <img src={"./img/bigBOFcom(360x120).png"} alt='bigBOF.com' width='180' height='60'/>
-        </div>
+      <div className="logoDiv">
+        <img className="logoImg" src={"./img/bigBOFcom(360x120).png"} alt='bigBOF.com' width='180' height='60'/>
+      </div>
+      <div className="navbarLinks">
         <div className="usernameDiv">
           {text[5][props.lang]}
         </div>
--- src/languages/text.js
+++ src/languages/text.js
@@ -2,52 +2,72 @@ const text = [
   {
     id : 0,
     english : "STOP!",
-    arabic : "!توقف"
+    arabic : "!توقف",
+    russian: "СТОП!",
+    czech: "STOP!"
   },
   {
     id : 1,
     english : "Please don't change anything here unless you know what you're doing.",
-    arabic : ".يرجى عدم تغيير أي شيء هنا إلا إذا كنت تعرف ما تفعله"
+    arabic : "يرجى عدم تغيير أي شيء هنا إلا إذا كنت تعرف ما تفعله.",
+    russian: "Пожалуйста, ничего не меняйте здесь, если вы не знаете, что делаете.",
+    czech: "Prosím, neměňte nic tady, pokud nevíte, co děláte."
   },
   {
     id : 2,
     english : "You might give hackers access to your account if you're not careful.",
-    arabic : ".قد تمنح المتسللين حق الوصول إلى حسابك إذا لم تكن حذراً"
+    arabic : "قد تمنح المتسللين حق الوصول إلى حسابك إذا لم تكن حذراً.ً",
+    russian: "Вы можете дать хакерам доступ к своей учетной записи, если вы не будете осторожны.",
+    czech: "Pokud nejste opatrní, můžete hackerům umožnit přístup k vašemu účtu."
   },
   {
     id : 3,
     english : "Good luck,",
-    arabic : "،حظا سعيدا"
+    arabic : "،حظا سعيدا",
+    russian: "Удачи,",
+    czech: "Hodně štěstí,"
   },
   {
     id : 4,
     english : "bigBOF.com",
-    arabic : "bigBOF.com"
+    arabic : "bigBOF.com",
+    russian: "bigBOF.com",
+    czech: "bigBOF.com"
   },
   {
     id : 5,
     english : "Username",
-    arabic : "اسم المستخدم"
+    arabic : "اسم المستخدم",
+    russian: "имя пользователя",
+    czech: "Uživatelské jméno"
   },
   {
     id : 6,
     english : "",
-    arabic : ""
+    arabic : "",
+    russian: "",
+    czech: ""
   },
   {
     id : 7,
     english : "",
-    arabic : ""
+    arabic : "",
+    russian: "",
+    czech: ""
   },
   {
     id : 8,
     english : "",
-    arabic : ""
+    arabic : "",
+    russian: "",
+    czech: ""
   },
   {
     id : 9,
     english : "",
-    arabic : ""
+    arabic : "",
+    russian: "",
+    czech: ""
   }
 ]
 
