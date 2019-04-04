8ec60fc3fa04f51f22d8922eeaaa20e3010dbe73
Shendidy <sherif@shendidy.com>
2019-04-04 06:09:51 +0100

    :lipstick: style: styling facebook feedback section

---
+++ src/components/FacebookFeedback/FacebookFeedback.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './FacebookFeedback.css';
+
+function FacebookFeedback() {
+  return(
+    <h3>What our winners said</h3>
+  )
+}
+
+export default FacebookFeedback
--- src/components/MainRightBody/MainRightBody.js
+++ src/components/MainRightBody/MainRightBody.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Ipsum from '../Ipsum/Ipsum';
+import FacebookFeedback from '../FacebookFeedback/FacebookFeedback';
 import './MainRightBody.css';
 
 class MainRightBody extends Component {
   render() {
     return (
       <div>
-        <div className="facebook-winners-comments">
+        <div className="facebook-winners-feedback">
+          <FacebookFeedback />
         </div>
         <Ipsum />
       </div>
